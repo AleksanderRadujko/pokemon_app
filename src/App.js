f37aa7a8cc6721a801a14405d7022d8eec753598
unknown <radujko.aleksander1@gmail.com>
2021-11-18 10:24:22 +0100

    finished project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+
 import './App.css';
 
+const fetchPokemonsUrl = "https://pokeapi.co/api/v2/pokemon?limit=10";
+
 function App() {
+  const [pokemons, setPokemons] = useState();
+  const [imgURL, setImgURL] = useState("");
+  const [pokemonName, setPokemonName] = useState("bulbasaur");
+
+  const getPokemons = async () => {
+    const response = await fetch(fetchPokemonsUrl);
+    const responseJson = await response.json();
+    setPokemons(responseJson.results);
+  }
+
+  const getPokemon = async (pokemon = "bulbasaur") => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const responseJson = await response.json();
+    setImgURL(responseJson.sprites.other.home.front_default);
+  }
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    await getPokemon(e.target.value);
+    setPokemonName(e.target.value);
+  }
+
+  useEffect(() => {
+    getPokemons();
+    getPokemon();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="navbar">
+        <img src="images/pokeball.png" alt="logo" />
+        <h1>pokémon.app</h1>
+      </div>
+
+      <main>
+        <form className="pokemons" >
+          {pokemons && pokemons.map((pokemon, idx) => {
+            return(
+              <input 
+                key={idx}
+                type="button"
+                value={pokemon.name}
+                onClick={handleFormSubmit}
+              />
+            );
+          })}
+        </form>
+        <div className="pokemon">
+          <img src={imgURL} alt="pokemonLogo" />
+          <h2>{pokemonName}</h2>
+        </div>
+      </main>
     </div>
   );
 }
